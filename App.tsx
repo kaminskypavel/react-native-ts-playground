cae2efe557bcbad331e0786703e7a02fb4ea4813
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-02-11 01:03:49 +0200

    feat(notifee): integrate instant reply

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import {Button, StyleSheet, View} from 'react-native';
-import notifee from '@notifee/react-native';
+import notifee, {EventType} from '@notifee/react-native';
+
+notifee.onForegroundEvent(async ({type, detail}) => {
+    if (type === EventType.ACTION_PRESS && detail.pressAction.id) {
+        console.log('User pressed an action with the id: ', JSON.stringify(detail));
+        //{
+        // 	"input": "Blaaaah",
+        // 	"pressAction": {
+        // 		"id": "reply"
+        // 	},
+        //  .....
+        // }
+        await notifee.cancelNotification(detail.notification.id);
+        await notifee.cancelAllNotifications();
+
+    }
+});
+
 
 export default function App() {
-
     async function onDisplayNotification() {
         // Create a channel
         const channelId = await notifee.createChannel({
@@ -21,18 +37,23 @@ export default function App() {
             android: {
                 channelId,
                 sound: 'coin',
-                ongoing: true,
+                // ongoing: true,
                 vibrationPattern: [300, 500],
                 largeIcon: 'https://tinyfac.es/data/avatars/A7299C8E-CEFC-47D9-939A-3C8CA0EA4D13-200w.jpeg',
                 color: '#4caf50',
                 actions: [
                     {
-                        title: '<b>Dance</b> 💡',
-                        pressAction: {id: 'dance'},
+                        title: 'Reply 📝',
+                        pressAction: {id: 'reply'},
+                        input: {
+                            allowFreeFormInput: true, // set to false
+                            choices: ['Yes', 'No', 'Maybe'],
+                            placeholder: 'Reply to Sarah...',
+                        },
                     },
                     {
-                        title: '<p style="color: #f44336;"><b>Cry</b> &#128557;</p>',
-                        pressAction: {id: 'cry'},
+                        title: '<p style="color: #f44336;"><b>Dismiss</b> ✈</p>',
+                        pressAction: {id: 'dismiss'},
                     },
                 ],
             },
