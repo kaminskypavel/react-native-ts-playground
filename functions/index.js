35482633d706a136c1ee46bac4348c4ad457ee99
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-02-11 15:43:50 +0200

    refactor(functions): make onCall instead of onRequest

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,31 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
+//https://console.firebase.google.com/u/0/project/drive-shield/functions/logs
 const sendTestNotification = async (timeout) => new Promise(res => {
-        setTimeout(() => {
-            console.log('done in', timeout);
+        setTimeout(async () => {
+
+            const userRef = await admin
+                .firestore()
+                .collection("users")
+                .doc(`test-user`)
+                .get()
+
+            const {token, notification} = userRef.data();
+            console.log('sending', token, notification);
+            const response = await admin.messaging().sendToDevice(token, {
+                notification
+            });
+
+            console.log('fcm response', response);
             res();
         }, timeout * 1000)
     }
 );
 
-exports.pushToTest = functions.https.onRequest(async (request, response) => {
-    const {timeout} = request.query;
-    response.send(`notification will be sent in ${timeout}s`)
+exports.pushToTest = functions.https.onCall(async (data, context) => {
+    const {timeout = 0} = data.timeout;
     await sendTestNotification(timeout);
+    return {status: "ok", timeout}
 });
