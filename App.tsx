fe3770ea6d5d369131b391002b3c6c2041f67c62
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-02-11 13:40:13 +0200

    feat(firebase): register FCM before registerComponent as described here https://stackoverflow.com/questions/58582059/cant-get-setbackgroundmessagehandler-to-work

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Platform, Text} from 'react-native';
 import messaging from '@react-native-firebase/messaging';
+import notifee from "@notifee/react-native";
 import styled from "styled-components/native"
-import notifee from "@notifee/react-native"
 
 const RequestButton = styled.Button({
     margin: 20
@@ -36,6 +36,37 @@ async function requestPermission() {
     }
 }
 
+export const onMessageReceived = async (message) => {
+    const channelId = await notifee.createChannel({
+        id: 'orders',
+        name: 'Orders',
+    });
+
+    // Display a notification
+    // https://notifee.app/react-native/docs/android/appearance
+    await notifee.displayNotification({
+        title: '<p style="color: green;"><b>Styled HTMLTitle</span></p></b></p>',
+        body: JSON.stringify(message.data),
+        android: {
+            channelId,
+            // ongoing: true,
+            largeIcon: 'https://tinyfac.es/data/avatars/A7299C8E-CEFC-47D9-939A-3C8CA0EA4D13-200w.jpeg',
+            color: '#4caf50',
+            actions: [
+                {
+                    title: 'Reply 📝',
+                    pressAction: {id: 'reply'},
+                    input: {
+                        allowFreeFormInput: true, // set to false
+                        choices: ['Yes', 'No', 'Maybe'],
+                        placeholder: 'Reply to Sarah...',
+                    },
+                }
+            ],
+        },
+    });
+};
+
 
 export default function App() {
     const [token, setToken] = useState("NO-TOKEN");
@@ -47,56 +78,12 @@ export default function App() {
         console.log(fcmToken);
     };
 
-    const onMessageReceived = async (message) => {
-        const channelId = await notifee.createChannel({
-            id: 'orders',
-            name: 'Orders',
-        });
-
-        // Display a notification
-        // https://notifee.app/react-native/docs/android/appearance
-        await notifee.displayNotification({
-            title: '<p style="color: green;"><b>Styled HTMLTitle</span></p></b></p>',
-            body: JSON.stringify(message.data),
-            android: {
-                channelId,
-                // ongoing: true,
-                largeIcon: 'https://tinyfac.es/data/avatars/A7299C8E-CEFC-47D9-939A-3C8CA0EA4D13-200w.jpeg',
-                color: '#4caf50',
-                actions: [
-                    {
-                        title: 'Reply 📝',
-                        pressAction: {id: 'reply'},
-                        input: {
-                            allowFreeFormInput: true, // set to false
-                            choices: ['Yes', 'No', 'Maybe'],
-                            placeholder: 'Reply to Sarah...',
-                        },
-                    }
-                ],
-            },
-        });
-    };
-
-    useEffect(() => {
-        const unsubscribe = messaging().onMessage(async (message) => {
-            console.log(`onMessage`, message.data);
-            await onMessageReceived(message);
-        });
-
-        messaging().setBackgroundMessageHandler(async (message) => {
-            console.log(`background`, message.data);
-            await onMessageReceived(message);
-        });
-
-    }, []);
-
     return (
         <Container>
             <Text>{Platform.OS} {Platform.Version}</Text>
             <RequestButton title={"Request Token"} onPress={() => getToken()}/>
             <RequestTokenView> {token}</RequestTokenView>
-            <RequestButton title={"Local Notification"} onPress={() => onMessageReceived(null)}/>
+            <RequestButton title={"Local Notification"} onPress={() => onMessageReceived({data: {some: "data"}})}/>
         </Container>
     );
 }
--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
+import {AppRegistry} from 'react-native';
+import App, {onMessageReceived} from './App';
+import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
+
+
+
+messaging().onMessage(async (message) => {
+    console.log(`onMessage`, message.data);
+    await onMessageReceived(message);
+});
+
+messaging().setBackgroundMessageHandler(async (message) => {
+    console.log(`background`, message.data);
+    await onMessageReceived(message);
+});
+
 
 AppRegistry.registerComponent(appName, () => App);
