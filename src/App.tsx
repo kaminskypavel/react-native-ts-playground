1c7bef9645b5c795051c508e50cfdce86cc411b1
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-02-15 14:26:28 +0200

    feat: add a navigation between screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from "@react-navigation/stack";
-import {Text, View} from "react-native";
+import {Button, Text, View} from "react-native";
+import NotificationsScreen from "./Screens/Notifications";
 
 const Stack = createStackNavigator();
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
     return (
-        <View>
-            <Text>Home Screen111111111111111111111</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Notifications')}
+            />
         </View>
     );
 }
@@ -26,6 +31,8 @@ export default function App() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home">
                 <Stack.Screen name="Home" component={HomeScreen}/>
+                <Stack.Screen name="Details" component={DetailsScreen} />
+                <Stack.Screen name="Notifications" component={NotificationsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
