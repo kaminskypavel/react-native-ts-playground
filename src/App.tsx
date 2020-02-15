cbb7195ee927aa7123630a5423d68ed5a313b100
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-02-15 14:22:02 +0200

    deps: upgrade to react-native 0.61.5

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from "@react-navigation/stack";
-import NotificationsScreen from "./Screens/Notifications"
+import {Text, View} from "react-native";
 
 const Stack = createStackNavigator();
+
+function HomeScreen() {
+    return (
+        <View>
+            <Text>Home Screen111111111111111111111</Text>
+        </View>
+    );
+}
+
+function DetailsScreen() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Details Screen</Text>
+        </View>
+    );
+}
 
 export default function App() {
     return (
         <NavigationContainer>
-            <NotificationsScreen/>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen}/>
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
---
+++ src/Screens/InAppMessaging/index.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import React from "react";
+
+export function InAppMessaging() {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
